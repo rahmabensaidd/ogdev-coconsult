2feea824c67c96d1784e2c14ae9c87f189a9aa85
rahmabensaidd <bensaidrahma2024@gmail.com>
2025-02-11 12:09:19 +0100

    first commit

---
+++ OGDevs-CoConsult-/src/main/java/tn/esprit/examen/OGDevs_CoConsult/OGDevs_CoConsult.java
@@ -0,0 +1,17 @@
+package tn.esprit.examen.OGDevs_CoConsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableAspectJAutoProxy
+@EnableScheduling
+@SpringBootApplication
+public class OGDevs_CoConsult {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OGDevs_CoConsult.class, args);
+    }
+
+}
